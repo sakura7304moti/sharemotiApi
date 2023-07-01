1ce8c6a15084ffbc35da58278206c7f402d28c75
DESKTOP-41T63M9\sakur <sakura7304moti@gmail.com>
2023-07-01 11:06:56 +0900

    レコード存在チェックの処理を修正

--- src/const.py
+++ src/const.py
@@ -7,25 +7,25 @@ import json
 # プロジェクトの相対パス
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # 各保存先
 class Output:
     def __init__(self):
         self._base_path = base_path
 
     def sqlite_db(self):
-        return os.path.join(self._base_path, "sns.db")
-    
+        return os.path.join(self._base_path, "share.db")
+
 
 """
 Record interface
 """
-class WordListRecord:#名言集
-    def __init__(self,word:str,desc:str):
+
+
+class WordListRecord:  # 名言集
+    def __init__(self, word: str, desc: str):
         self.word = word
         self.desc = desc
 
     def __dict__(self):
-        return {
-            "word":self.word,
-            "desc":self.desc
-        }
+        return {"word": self.word, "desc": self.desc}
--- src/wordList.py
+++ src/wordList.py
@@ -2,11 +2,13 @@
 名言集のAPI
 """
 from . import const
+
 output = const.Output()
 """
 CREATE DB
 """
 import sqlite3
+
 # sns.dbを作成する
 # すでに存在していれば、それにアスセスする。
 dbname = output.sqlite_db()
@@ -29,16 +31,58 @@ cur.execute(
             word STRING,
             desc STRING
             )
-            """)
+            """
+)
 
 # データベースへコミット。これで変更が反映される。
 conn.commit()
 conn.close()
 
 """
-INSERT
+INSERT and UPDATE
 """
-def insert(word:str='',desc:str=''):
+
+
+def save(word: str = "", desc: str = ""):
+    # データベースに接続する
+    conn = sqlite3.connect(dbname)
+    cursor = conn.cursor()
+
+    if word != "":
+        # レコードの存在をチェックするためのクエリを作成する
+        check_query = "SELECT * FROM wordList WHERE word = :word"
+        cursor.execute(check_query, {"word": word})
+        result = cursor.fetchall()
+
+        if not result:
+            # 存在しないレコードなら追加する
+            query = "INSERT INTO wordList (word, desc) VALUES (:word, :desc)"
+            args = {"word": word, "desc": desc}
+            cursor.execute(query, args)
+        else:
+            # 存在するレコードなら更新する
+            query = "UPDATE wordList SET desc = :desc WHERE word = :word"
+            args = {"word": word, "desc": desc}
+            cursor.execute(query, args)
+
+        # 変更をコミットし、接続を閉じる
+        conn.commit()
+        conn.close()
+
+        return {
+            "insert": not result,
+            "update": bool(result),
+        }
+    else:
+        return {"insert": False, "update": False}
+
+
+"""
+DELETE
+"""
+
+
+def delete(word: str = "", desc: str = ""):
     # データベースに接続する
     conn = sqlite3.connect(dbname)
     cursor = conn.cursor()
@@ -49,45 +93,43 @@ def insert(word:str='',desc:str=''):
     cursor.execute(check_query)
     result = cursor.fetchall()
 
-    #存在しないレコードなら追加する
-    if result is None:
-        query = "INSERT INTO wordList (word,desc) VALUES (:word,:desc)"
-        args = {
-            'word':word,
-            'desc':desc
-        }
-        # レコードを追加する
-        cursor.execute(query,args)
+    # 存在するレコードなら削除する
+    if result != []:
+        query = "DELETE FROM wordlist WHERE word = :word and desc = :desc)"
+        args = {"word": word, "desc": desc}
+        # レコードを削除する
+        cursor.execute(query, args)
 
     # 変更をコミットし、接続を閉じる
     conn.commit()
     conn.close()
-    
-    #追加したらtrue,すでに存在したらfalse
-    return True if result is None else False
+
+    # 削除したらtrue,存在しなかったらfalse
+    return True if result != [] else False
+
 
 """
 SELECT
 """
-def search(word:str='',desc:str=''):
+
+
+def search(word: str = "", desc: str = ""):
     # データベースに接続する
     conn = sqlite3.connect(dbname)
     cursor = conn.cursor()
 
     # クエリー設定
     query = "SELECT * FROM wordList WHERE 1 = 1 "
-    if word != '':
+    if word != "":
         query = query + "and word like :word "
-    if desc != '':
+    if desc != "":
         query = query + "and desc like :desc "
 
-    args = {
-        'word' : f'%{word}%',
-        'desc' : f'%{desc}%'
-    }
+    args = {"word": f"%{word}%", "desc": f"%{desc}%"}
+    print("query", query)
 
     # SELECTクエリを実行
-    cursor.execute(query,args)
+    cursor.execute(query, args)
     results = cursor.fetchall()
 
     # 結果を表示
